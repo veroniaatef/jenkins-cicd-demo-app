88a3ebd389de30b2badeb6763d5c3b4c6338dc42
Veronia Atef <veroniaatef16@gmail.com>
2026-06-04 20:39:16 +0300

    Fix server startup

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 const express = require("express");
 
 const app = express();
@@ -13,4 +15,6 @@ app.get("/", (req, res) => {
   `);
 });
 
-module.exports = app;
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
