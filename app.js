455f2f166319c26e817b130a47f8f32310efe4f1
Veronia Atef <veroniaatef16@gmail.com>
2026-06-04 15:45:11 +0300

    Initial Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(`
+    <html>
+      <body>
+        <h1>Hello from Jenkins Pipeline</h1>
+        <p>Build, Test and Deploy with Docker</p>
+      </body>
+    </html>
+  `);
+});
+
+module.exports = app;
